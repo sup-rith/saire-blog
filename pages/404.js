f3d3a63a6b6239bd266f6e95b8545fd26b42a0b8
Suprith Aireddy <aireddysuprith@gmail.com>
2020-08-01 16:26:01 -0500

    Next.js setup blog.

---
+++ pages/404.js
@@ -0,0 +1,9 @@
+import Layout from "../components/layout";
+
+export default function Custom404() {
+  return (
+    <Layout>
+      <h1>404 😵 - This page that doesn't exist </h1>
+    </Layout>
+  )
+};
